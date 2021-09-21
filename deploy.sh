10810e24628d5fdda9d0846fe9c25390b9e1564e
ElderPintto <eldersilva@gmail.com>
2021-09-21 23:52:14 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,10 @@ git init
 git add -A
 git commit -m 'deploy'
 
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ElderPintto/my-app-todo.git master:gh-pages
+git push -f git@github.com:elderpintto.github.io/my-app-todo.git main:gh-pages
 
 cd -
