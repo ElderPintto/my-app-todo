f803958910ac1d8731dc00812b397fc9e1b0b81f
ElderPintto <eldersilva@gmail.com>
2021-09-22 00:15:09 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,16 +6,11 @@ set -e
 # build
 npm run build
 
-# navigate into the build output directory
 cd dist
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
-
 git push -f https://ElderPintto.github.io/my-app-todo.git master:gh-pages
 
 cd -
