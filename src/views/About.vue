660364506dd72a28e37c966cc3e9a791eac28c8a
ElderPintto <eldersilva@gmail.com>
2021-09-21 23:14:11 +0100

    todo

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,42 @@
 <template>
-  <div class="about">
+  <div class="about pa-6">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe reiciendis unde sequi fuga ipsa minima impedit ad, nemo ratione ea aliquam deserunt praesentium quia officia qui laborum amet accusamus explicabo!
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe reiciendis unde sequi fuga ipsa minima impedit ad, nemo ratione ea aliquam deserunt praesentium quia officia qui laborum amet accusamus explicabo!
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe reiciendis unde sequi fuga ipsa minima impedit ad, nemo ratione ea aliquam deserunt praesentium quia officia qui laborum amet accusamus explicabo!
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe reiciendis unde sequi fuga ipsa minima impedit ad, nemo ratione ea aliquam deserunt praesentium quia officia qui laborum amet accusamus explicabo!
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe reiciendis unde sequi fuga ipsa minima impedit ad, nemo ratione ea aliquam deserunt praesentium quia officia qui laborum amet accusamus explicabo!
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe reiciendis unde sequi fuga ipsa minima impedit ad, nemo ratione ea aliquam deserunt praesentium quia officia qui laborum amet accusamus explicabo!
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe reiciendis unde sequi fuga ipsa minima impedit ad, nemo ratione ea aliquam deserunt praesentium quia officia qui laborum amet accusamus explicabo!
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe reiciendis unde sequi fuga ipsa minima impedit ad, nemo ratione ea aliquam deserunt praesentium quia officia qui laborum amet accusamus explicabo!
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe reiciendis unde sequi fuga ipsa minima impedit ad, nemo ratione ea aliquam deserunt praesentium quia officia qui laborum amet accusamus explicabo!
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe reiciendis unde sequi fuga ipsa minima impedit ad, nemo ratione ea aliquam deserunt praesentium quia officia qui laborum amet accusamus explicabo!
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe reiciendis unde sequi fuga ipsa minima impedit ad, nemo ratione ea aliquam deserunt praesentium quia officia qui laborum amet accusamus explicabo!
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe reiciendis unde sequi fuga ipsa minima impedit ad, nemo ratione ea aliquam deserunt praesentium quia officia qui laborum amet accusamus explicabo!
+    </p>
+    
   </div>
 </template>
