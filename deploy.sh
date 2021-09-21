12bdff92c96ddc0d418930e14ce4fe0e58be21c5
ElderPintto <eldersilva@gmail.com>
2021-09-21 23:35:42 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -16,9 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:elderpintto/elderpintto.github.io.git main
-
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:elderpintto/my-app-todo.git main:gh-pages
 
--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     'vuetify'
   ],
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/dist/'
+    ? '/my-app-todo/'
     : '/'
   }
 }
